392138e52c21a559fa15327f5b99029f3ba924b8
thomas-ayissi <thomas8sea@gmail.com>
2022-02-23 14:31:13 +0100

    V23Fev2022d

--- model/theSectionModel.php
+++ model/theSectionModel.php
@@ -37,11 +37,35 @@ Quand il existe la variable GET idsection*/
 
 // set the PDO error mode to exception
 
-if(isset($_GET['idsection'])
-{
+if(isset($_GET['idsection']){
 
+        try {
+            $db = new PDO($, $, $);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch (PDOException $) {
+            echo 'Échec lors de la connexion : ' . $e->getMessage();
+      }
+
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=mysql", $user, $pass);
+    $query = "CREATE VIEW theSectionModel AS 
+       select option_name,option_value from db1.options 
+      UNION ALL 
+       select option_name,option_value from db2.options;"
+
+    $pdo->beginTransaction();
+    $pdo->exec($query);
+    $result = $pdo->commit();
+    if ($result) {
+        print_r("COMMIT OK!");
+    } else {
+        print_r("COMMIT FALSE");
+        $pdo->rollBack();
+    }
 
 }
+
+
+
 /*Création d'une vue pour afficher les détails de la section et
 les 20 derniers articles de cette section (comme l'accueil)*/
 
